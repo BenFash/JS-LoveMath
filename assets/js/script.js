63c95faf1df546a69c89a43383483ea0870b38f9
Ben Fashan <benfashan71@msn.com>
2024-01-25 12:19:38 +0000

    Add: multiplication game code

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,6 +29,8 @@ function runGame(gameType) {
 
   if (gameType === "addition") {
     displayAdditionQuestion(num1, num2);
+  } else if (gameType === "multiply") {
+    displayMultiplyQuestion(num1, num2);
   } else {
     alert(`Unkown game type: ${gameType}`);
     throw `Unkown game type: ${gameType}. Aborting`;
@@ -68,6 +70,8 @@ function calculateCorrectAnswer() {
 
   if (operator === "+") {
     return [operand1 + operand2, "addition"];
+  } else if (operator === "x") {
+    return [operand1 + operand2, "multiply"];
   } else {
     alert(`Unimplemeneted operator ${operator}`);
     throw `Unimplemeneted operator ${operator}. Aborting`;
@@ -96,8 +100,12 @@ function displayAdditionQuestion(operand1, operand2) {
   document.getElementById("operator").textContent = "+";
 }
 
-function displaySubtractQuestion() {}
+function displaySubtractQuestion(operand1, operand2) {}
 
-function displayMultiplyQuestion() {}
+function displayMultiplyQuestion(operand1, operand2) {
+  document.getElementById("operand1").textContent = operand1;
+  document.getElementById("operand2").textContent = operand2;
+  document.getElementById("operator").textContent = "x";
+}
 
 function displayDivisionQuestion() {}
