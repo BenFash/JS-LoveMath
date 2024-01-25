d230e544bd89dccdfa2d598b18489123e19aa79d
Ben Fashan <benfashan71@msn.com>
2024-01-25 13:53:50 +0000

    Add: Division game code

--- assets/js/script.js
+++ assets/js/script.js
@@ -47,6 +47,8 @@ function runGame(gameType) {
     displayMultiplyQuestion(num1, num2);
   } else if (gameType === "subtract") {
     displaySubtractQuestion(num1, num2);
+  } else if (gameType === "division") {
+    displayDivisionQuestion(num1, num2);
   } else {
     alert(`Unkown game type: ${gameType}`);
     throw `Unkown game type: ${gameType}. Aborting`;
@@ -90,6 +92,8 @@ function calculateCorrectAnswer() {
     return [operand1 * operand2, "multiply"];
   } else if (operator === "-") {
     return [operand1 - operand2, "subtract"];
+  } else if (operator === "/") {
+    return [operand1 / operand2, "division"];
   } else {
     alert(`Unimplemeneted operator ${operator}`);
     throw `Unimplemeneted operator ${operator}. Aborting`;
@@ -132,4 +136,9 @@ function displayMultiplyQuestion(operand1, operand2) {
   document.getElementById("operator").textContent = "x";
 }
 
-function displayDivisionQuestion() {}
+function displayDivisionQuestion(operand1, operand2) {
+  operand1 = operand1 * operand2;
+  document.getElementById("operand1").textContent = operand1;
+  document.getElementById("operand2").textContent = operand2;
+  document.getElementById("operator").textContent = "/";
+}
